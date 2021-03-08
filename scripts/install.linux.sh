39a3d5d86d76c52f74a060829221716bf006abc9
sam bacha <sam@freighttrust.com>
2021-03-07 21:22:50 -0800

    build(cmake): update script

--- scripts/install.linux.sh
+++ scripts/install.linux.sh
@@ -1,10 +1,14 @@
+#!/bin/sh
 CMAKE_MINOR=3.5
 CMAKE=cmake-${CMAKE_MINOR}.2-Linux-x86_64
 
+echo "Installing Ragel..."
 sudo apt-get --assume-yes update
-sudo apt-get --assume-yes install ragel valgrind
+sudo apt-get --assume-yes install ragel valgrind -qq
 wget https://cmake.org/files/v${CMAKE_MINOR}/${CMAKE}.tar.gz
 tar -xzf ${CMAKE}.tar.gz
 mv ${CMAKE} cmake-build
-./cmake-build/bin/cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON
+echo "CMake Build Starting..."
+#./cmake-build/bin/cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON
+./cmake-build/bin/cmake -H. -Bbuild -w
 ./cmake-build/bin/cmake --build build
